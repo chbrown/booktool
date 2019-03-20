d5717aab0398f5446d220d2eb52d9ab02ddbfc4c
Christopher Brown <io@henrian.com>
2019-03-20 01:38:00 -0400

    Add util.sanitize and corresponding tests

---
+++ booktool/util.py
@@ -0,0 +1,31 @@
+import re
+from string import punctuation
+
+
+def is_sanitized(string: str) -> bool:
+    return re.fullmatch(r"[0-9A-Za-z][-0-9A-Za-z_]+[0-9A-Za-z]", string)
+
+
+def sanitize(string: str) -> str:
+    # remove periods after initials; ensure a space follows
+    string = re.sub(r"\b([A-Z])(\. ?| )", r"\1 ", string)
+    # spell out ampersands / plus signs
+    string = re.sub(r"\s*[&+]\s*", r" and ", string)
+    # spell out at-sign
+    string = re.sub(r"\s*@\s*", r" at ", string)
+    # collapse hyphenations
+    string = re.sub(r"([A-Za-z])-([A-Za-z])", r"\1\2", string)
+    # replace separator punctuation with a hyphen
+    string = re.sub(r"\s*[!.:;?]\s*", r"-", string)
+    # replace parentheticals by separating with hyphen
+    string = re.sub(r"\s*\(([^)]*)\)\s*", r"-\1-", string)
+    string = re.sub(r"\s*\[([^]]*)\]\s*", r"-\1-", string)
+    # replace any other remaining punctuation (within reason) with a space
+    string = re.sub(r"""["$%',]""", r" ", string)
+    # replace whitespace with underscore
+    string = re.sub(r"\s+", r"_", string)
+    # remove leading/trailing punctuation
+    string = string.strip(punctuation)
+    # done!
+    assert is_sanitized(string), f"{string!r} failed sanitization"
+    return string
---
+++ tests/test_util.py
@@ -0,0 +1,24 @@
+from booktool.util import is_sanitized, sanitize
+
+# data maps raw value(s) to the proper sanitized output
+data = [
+    (["J. K. Rowling", "J.K.Rowling", "J K Rowling"], "J_K_Rowling"),
+    (["Jon Kabat-Zinn"], "Jon_KabatZinn"),
+    (["And Yet..."], "And_Yet"),
+    (["Infinite Jest (Abridged)"], "Infinite_Jest-Abridged"),
+    (
+        ["The Information: A History, a Theory, a Flood"],
+        "The_Information-A_History_a_Theory_a_Flood",
+    ),
+]
+
+
+def test_data():
+    for _, output in data:
+        assert is_sanitized(output)
+
+
+def test_sanitize():
+    for inputs, output in data:
+        for value in inputs:
+            assert sanitize(value) == output
