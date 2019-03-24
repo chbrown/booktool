5dbe918fb09f87c36725e0f6e78c549b3aeffcbe
Christopher Brown <io@henrian.com>
2019-03-24 13:24:29 -0400

    Expand fix-track-numbers into canonicalize command

--- booktool/__main__.py
+++ booktool/__main__.py
@@ -3,12 +3,20 @@ Booktool CLI
 """
 from typing import List
 import logging
+import os
 
 import click
 
 import booktool
 from booktool.audio import find_audio
-from booktool.audio.track import get_duration, get_track_number, set_track_number
+from booktool.audio.track import (
+    get_album,
+    get_artist,
+    get_duration,
+    get_track_number,
+    set_track_number,
+)
+from booktool.util import chmod, move, sanitize
 
 logger = logging.getLogger(booktool.__name__)
 
@@ -25,14 +33,46 @@ def cli(verbose: int):
 
 
 @cli.command()
-@click.argument("paths", type=click.Path(exists=True, file_okay=False), nargs=-1)
-def fix_track_numbers(paths: List[str]):
+@click.argument("paths", type=click.Path(exists=True), nargs=-1)
+@click.option(
+    "-d",
+    "--destination",
+    type=click.Path(exists=True, file_okay=False),
+    default=os.getcwd(),
+    show_default=True,
+    help="Destination directory",
+)
+@click.option(
+    "-n",
+    "--dry-run",
+    is_flag=True,
+    help="Don't actually do anything, just log any changes that would be made",
+)
+def canonicalize(paths: List[str], destination: str, dry_run: bool):
     """
-    Fix track numbers in audiobook(s)
+    Rearrange audio files into canonical structure.
+
+    1. restructure directories and filenames
+    2. fix file permissions
+    2. fix track numbers
     """
     for path in find_audio(*paths):
+        artist = sanitize(get_artist(path))
+        album = sanitize(get_album(path))
+        _, ext = os.path.splitext(os.path.basename(path))
         track_number, total_tracks = get_track_number(path)
+        part_width = len(str(total_tracks))
+
+        new_filename = f"{track_number:0{part_width}}{ext}".lower()
+        new_filepath = os.path.join(destination, artist, album, new_filename)
+
+        # move to destination
+        path = move(path, new_filepath, dry_run=dry_run)
+        # fix permissions on files
+        chmod(path, 0o644, dry_run=dry_run)
+        # fix track numbers in audio
         set_track_number(path, track_number, total_tracks)
+        # ignore xattrs; they're dropped when syncing to cloud storage anyway
 
 
 @cli.command()
