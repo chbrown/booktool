8c3f079fd4c5b86b7c6dabc2ba0139a6066b605e
Christopher Brown <io@henrian.com>
2019-03-24 13:23:09 -0400

    Add chmod and move

--- booktool/util.py
+++ booktool/util.py
@@ -1,5 +1,9 @@
+import os
 import re
+import logging
 from string import punctuation
+
+logger = logging.getLogger(__name__)
 
 
 def is_sanitized(string: str) -> bool:
@@ -29,3 +33,34 @@ def sanitize(string: str) -> str:
     # done!
     assert is_sanitized(string), f"{string!r} failed sanitization"
     return string
+
+
+def chmod(path: str, mode: int = 0o644, dry_run: bool = False):
+    """
+    Set the access permissions of the file/directory at `path` to `mode`.
+    Checks first if the file's mode already matches the given mode, in which
+    case it does nothing.
+    """
+    current_mode = os.stat(path).st_mode & 0o777
+    # short-circuit if current mode matches
+    if current_mode != mode:
+        logger.info("%s: chmod %o -> %o", path, current_mode, mode)
+        if not dry_run:
+            os.chmod(path, mode)
+
+
+def move(src: str, dst: str, dry_run: bool = False) -> str:
+    """
+    Move `src` to `dst`. If `src` and `dst` refer to the same path, do nothing.
+    If they are different and `dst` already exists, raise a FileExistsError.
+    """
+    # short-circuit if src and dst refer to the same thing
+    # os.path.samefile(src, dst) doesn't work because it calls os.stat on each argument
+    if os.path.realpath(src) != os.path.realpath(dst):
+        if os.path.exists(dst):
+            raise FileExistsError(dst)
+        logger.info("%s: move -> %s", src, dst)
+        if not dry_run:
+            os.renames(src, dst)
+            return dst
+    return src
