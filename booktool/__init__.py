986d65c4e00722437a4a4103366943e298ddc32b
Christopher Brown <io@henrian.com>
2020-08-20 11:19:50 -0400

    Add isbn13to10 helper

--- booktool/__init__.py
+++ booktool/__init__.py
@@ -6,3 +6,16 @@ try:
     __version__ = pkg_resources.get_distribution("booktool").version
 except Exception:
     pass
+
+
+def isbn13to10(isbn13: str) -> str:
+    """
+    Convert ISBN-13 to ISBN-10 (without checking validity of input).
+    """
+    isbn9 = isbn13[3:12]
+    # for the checksum, isbn9 is zipped with [10, 9, ... 2]
+    weights = range(10, 1, -1)
+    checksum: int = sum(weight * int(digit) for weight, digit in zip(weights, isbn9))
+    checkdigit: int = 11 - (checksum % 11)
+    checkdigit_str: str = "X" if checkdigit == 10 else str(checkdigit)
+    return isbn9 + checkdigit_str
